f877273339300b72f441bc6e12e7e1be3b866843
Alex Dipasquale <aledipas@cisco.com>
2019-07-12 11:25:10 -0400

    nazjatar whitelisting

--- BeQuiet.lua
+++ BeQuiet.lua
@@ -11,6 +11,13 @@ if ISLANDS == nil then
 	ISLANDS = 0
 end
 
+if NAZJATAR == nil then
+	NAZJATAR = 0
+end
+
+--Define nazjatar zone name
+nazjatar = "Nazjatar"
+
 --Define withered army training zones
 withered =	{
 		"Temple of Fal'adora",
@@ -49,7 +56,8 @@ function f:OnEvent(event, addon)
 				--Block the talking head unless we have a whitelisted condition
 				if not (mainZoneName == 'Ashran' and ASHRAN == 1) and
 					not (has_value(islands, mainZoneName) and ISLANDS == 1) and
-					not (has_value(withered, zoneName)) then
+					not (has_value(withered, zoneName)) and
+					not (mainZoneName == 'Nazjatar' and NAZJATAR == 1) then
 					--Close the talking head
 					TalkingHeadFrame_CloseImmediately()
 				end
@@ -93,6 +101,14 @@ local function MyAddonCommands(arg)
 			ISLANDS = 0
 			print('Talking heads will now be blocked in Island Expeditions')
 		end
+	elseif arg == 'nazjatar' then
+		if NAZJATAR == 0 then
+			NAZJATAR = 1
+			print('Talking heads will now be allowed in Nazjatar')
+		elseif NAZJATAR == 1 then
+			NAZJATAR = 0
+			print('Talking heads will now be blocked in Nazjatar')
+		end
 	else
 		if ENABLED == 0 then
 			print('BeQuiet is currently disabled')
@@ -107,6 +123,11 @@ local function MyAddonCommands(arg)
 				print('Talking heads are currently blocked in Island Expeditions')
 			elseif ISLANDS == 1 then
 				print('Talking heads are currently allowed in Island Expeditions')
+			end
+			if NAZJATAR == 0 then
+				print('Talking heads are currently blocked in Nazjatar')
+			elseif NAZJATAR == 1 then
+				print('Talking heads are currently allowed in Nazjatar')
 			end
 		end
 		print(' ')
