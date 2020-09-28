7dca38194d73d2dc17258bf0c79af38763284d0a
Xorag <adipasquale2190@gmail.com>
2020-09-28 18:46:35 -0400

    3.0 rewrite for SL

--- BeQuiet.lua
+++ BeQuiet.lua
@@ -4,7 +4,7 @@ if ENABLED == nil then
 end
 
 if VERBOSE == nil then
-	VERBOSE = 0
+	VERBOSE = 1
 end
 
 --Default whitelist includes the withered army training zones from legion and island expeditions from BFA
@@ -47,12 +47,20 @@ function f:OnEvent(event, addon)
 					--Close the talking head
 					TalkingHeadFrame_CloseImmediately()
 					if VERBOSE == 1 then
-						print("BeQuiet blocked a talking head. /bq verbose to turn these messages off.")
+						print("BeQuiet blocked a talking head! /bq verbose to turn this alert off.")
 					end
 				end
 			end
 		end)
 	self:UnregisterEvent(event)
+	end
+end
+
+function removeFirst(tbl, val)
+	for i, v in ipairs(tbl) do
+		if v == val then
+			return table.remove(tbl, i)
+		end
 	end
 end
 
@@ -67,70 +75,91 @@ function has_value (tab, val)
 end
 
 --Slash command function
-local function MyAddonCommands(arg1, arg2, arg3)
-	if arg1 == 'off' then
+function MyAddonCommands(args)
+	if args == 'off' then
 		ENABLED = 0
 		print('BeQuiet disabled - now allowing talking heads.')
-	elseif arg1 == 'on' then
+	end
+
+	if args == 'on' then
 		ENABLED = 1
 		print('BeQuiet enabled - now blocking talking heads except for whitelisted zones.')
-	elseif arg1 == 'whitelist' then
-		if arg2 == nil then
-			print ('Options:')
-			print('add *Zone Name* - adds a zone (Orgrimmar) or sub-zone (Valley of Strength) name to the whitelist. Case sensitive!')
-			print('remove *Zone Name* - removes a zone (Orgrimmar) or sub-zone (Valley of Strength) name from the whitelist. Case sensitive!')
-			print('reset - resets the whitelist to the default zones. (Withered Army Training & Island Expeditions)')
-			print('show - show the current zones on the whitelist.')
-		elseif arg2 == 'add' then
-			if arg3 ~= nil then
-				table.insert(WHITELIST, arg3)
-				print(arg3 .. ' added to the whitelist.')
-			else
-				print('Provide a zone name (Orgimmar) or sub-zone name (Valley of Strength) to add to the whitelist. Case sensitive!')
-			end
-		elseif arg2 == 'remove' then
-			if arg3 ~= nil then
-				if has_value(WHITELIST, arg3) then
-					table.remove(WHITELIST, arg3)
-					print(arg3 .. ' removed from the whitelist.')
-				else
-					print('Zone name was not found in the whitelist.')
-				end
-			else
-				print('Provide a zone name or sub-zone name to be removed from the whitelist. Case sensitive!')
-			end
-		elseif arg2 == 'reset' then
-			WHITELIST = {
-				"Temple of Fal'adora",
-				"Falanaar Tunnels",
-				"Shattered Locus",
-				"Crestfall",
-				"Snowblossom Village",
-				"Havenswood",
-				"Jorundall",
-				"Molten Cay",
-				"Un'gol Ruins",
-				"The Rotting Mire",
-				"Whispering Reef",
-				"Verdant Wilds",
-				"The Dread Chain",
-				"Skittering Hollow"
-			}
-			print('Whitelist has been reset to default.')
-		elseif arg2 == 'show' then
-			print(table.concat(WHITELIST, ', '))
+	end
+
+	if args == 'whitelist currentzone' then
+		zone = GetZoneText()
+		if has_value(WHITELIST, zone) then
+			removeFirst(WHITELIST, zone)
+			print(zone .. ' removed from the whitelist.')
+		else
+			table.insert(WHITELIST, zone)
+			print(zone .. ' added to the whitelist.')
 		end
-	elseif arg1 == 'verbose' then
+	end
+
+	if args == 'whitelist currentsubzone' then
+		zone = GetSubZoneText()
+		if has_value(WHITELIST, zone) then
+			removeFirst(WHITELIST, zone)
+			print(zone .. ' removed from the whitelist.')
+		else
+			table.insert(WHITELIST, zone)
+			print(zone .. ' added to the whitelist.')
+		end
+	end
+
+	if args == 'reset' then
+		WHITELIST = {
+			"Temple of Fal'adora",
+			"Falanaar Tunnels",
+			"Shattered Locus",
+			"Crestfall",
+			"Snowblossom Village",
+			"Havenswood",
+			"Jorundall",
+			"Molten Cay",
+			"Un'gol Ruins",
+			"The Rotting Mire",
+			"Whispering Reef",
+			"Verdant Wilds",
+			"The Dread Chain",
+			"Skittering Hollow"
+		}
+		print('Whitelist has been reset to default.')
+	end
+
+	if args == 'show' then
+		print(table.concat(WHITELIST, ', '))
+	end
+
+	if args == 'verbose' then
 		if VERBOSE == 0 then
 			VERBOSE = 1
 			print('Verbose mode enabled. A chat message will print when a talking head is blocked.')
 		elseif VERBOSE == 1 then
 			VERBOSE = 0
-			print('Verbose mode disabled. A chat message will not print when a talking head is blocked.')
+			print('Verbose mode disabled.')
 		end
-	elseif arg1 == nil then
+	end
+
+	if args == 'whitelist' then
+		print('whitelist (currentzone | currentsubzone) - toggle whitelisting for the current major zone (Orgrimmar) or sub-zone (Valley of Strength).')
+	end
+
+	if args == '' then
 		print('BeQuiet version ' .. version)
-		print('Options: on | off | whitelist | verbose')
+		print('Options: on | off | verbose | whitelist | reset | show')
+		print('-----')
+		if ENABLED == 1 then
+			print('BeQuiet is currently enabled.')
+		elseif ENABLED == 0 then
+			print('BeQuiet is currently disabled.')
+		end
+		if VERBOSE == 1 then
+			print('Verbose mode is currently enabled.')
+		elseif VERBOSE == 0 then
+			print('Verbose mode is currently disabled.')
+		end
 	end
 end
 
