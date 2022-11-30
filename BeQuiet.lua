7ad188c712993f1f786b2dc8a2954e1743477b48
Hawkwood <dazerdude@gmail.com>
2022-11-29 19:00:31 -0500

    Added a blacklist function

--- BeQuiet.lua
+++ BeQuiet.lua
@@ -18,9 +18,15 @@ WL_DEFAULT = {
 	"Torghast, Tower of the Damned"
 }
 
+BL_DEFAULT = {}
+
 --Initialize config variables if they are not saved
 if ENABLED == nil then
 	ENABLED = 1
+end
+
+if VO_ENABLED == nil then
+	VO_ENABLED = 0
 end
 
 if VERBOSE == nil then
@@ -30,6 +36,11 @@ end
 --Default whitelist includes the withered army training zones from legion and island expeditions from BFA
 if WHITELIST == nil then
 	WHITELIST = WL_DEFAULT
+end
+
+-- BLACKLIST defaults to empty. This preserves off behavior from before.
+if BLACKLIST == nil then
+	BLACKLIST = BL_DEFAULT
 end
 
 --Create the frame
@@ -43,16 +54,23 @@ function close_head()
 	if ENABLED == 1 then
 		--Block the talking head unless its in the whitelist
 		if (has_value(WHITELIST, subZoneName) ~= true and has_value(WHITELIST, zoneName) ~= true) then
-			--Close the talking head
-			--TalkingHeadFrame:CloseImmediately(); pre 10.0.7
-			TalkingHeadFrame:Hide()
-			if TalkingHeadFrame.voHandle ~= nil then
-				C_Timer.After(0.025, function() StopSound(TalkingHeadFrame.voHandle) end);
-			end
-			if VERBOSE == 1 then
-				print("BeQuiet blocked a talking head! /bq verbose to turn this alert off.")
-			end
+			block_head();
 		end
+	--If disabled, check blacklist
+	elseif (has_value(BLACKLIST, subZoneName) or has_value(BLACKLIST, zoneName)) then
+		block_head();
+	end
+end
+
+function block_head()
+	--Close the talking head
+	--TalkingHeadFrame:CloseImmediately(); pre 10.0.7
+	TalkingHeadFrame:Hide()
+	if TalkingHeadFrame.voHandle ~= nil and VO_ENABLED == 0 then
+		C_Timer.After(0.025, function() StopSound(TalkingHeadFrame.voHandle) end);
+	end
+	if VERBOSE == 1 then
+		print("BeQuiet blocked a talking head! /bq verbose to turn this alert off.")
 	end
 end
 
@@ -83,7 +101,7 @@ end
 
 --Slash command function
 function MyAddonCommands(args)
-	allow_msg = 'BeQuiet disabled - now allowing talking heads.'
+	allow_msg = 'BeQuiet disabled - now allowing talking heads except for blacklisted zones.'
 	block_msg = 'BeQuiet enabled - now blocking talking heads except for whitelisted zones.'
 
 	if args == 'off' then
@@ -127,19 +145,44 @@ function MyAddonCommands(args)
 			print(zone .. ' added to the whitelist.')
 		end
 	end
+	
+	if args == 'blacklist currentzone' then
+		zone = GetZoneText()
+		if has_value(BLACKLIST, zone) then
+			removeFirst(BLACKLIST, zone)
+			print(zone .. ' removed from the blacklist.')
+		else
+			table.insert(BLACKLIST, zone)
+			print(zone .. ' added to the blacklist.')
+		end
+	end
+
+	if args == 'blacklist currentsubzone' then
+		zone = GetSubZoneText()
+		if has_value(BLACKLIST, zone) then
+			removeFirst(BLACKLIST, zone)
+			print(zone .. ' removed from the blacklist.')
+		else
+			table.insert(BLACKLIST, zone)
+			print(zone .. ' added to the blacklist.')
+		end
+	end
 
 	if args == 'delete' then
 		WHITELIST = {}
-		print('Whitelist has been deleted.')
+		BLACKLIST = {}
+		print('Whitelist and blacklist have been deleted.')
 	end
 
 	if args == 'reset' then
 		WHITELIST = WL_DEFAULT
-		print('Whitelist has been reset to default.')
+		BLACKLIST = BL_DEFAULT
+		print('Whitelist and blacklist have been reset to default.')
 	end
 
 	if args == 'show' then
-		print(table.concat(WHITELIST, ', '))
+		print('whitelist: ' .. table.concat(WHITELIST, ', '))
+		print('blacklist: ' .. table.concat(BLACKLIST, ', '))
 	end
 
 	if args == 'verbose' then
@@ -151,14 +194,42 @@ function MyAddonCommands(args)
 			print('Verbose mode disabled.')
 		end
 	end
+	
+	if args == 'vo on' then
+		VO_ENABLED = 1
+		print('VoiceOver enabled when talking head frame hidden')
+	end
+
+	if args == 'vo off' then
+		VO_ENABLED = 0
+		print('VoiceOver disabled when talking head frame hidden')
+	end
+
+	if args == 'vo toggle' then
+		if VO_ENABLED == 1 then
+			VO_ENABLED = 0
+			print('VoiceOver disabled when talking head frame hidden')
+		else
+			VO_ENABLED = 1
+			print('VoiceOver enabled when talking head frame hidden')
+		end
+	end
 
 	if args == 'whitelist' then
 		print('whitelist (currentzone | currentsubzone) - toggle whitelisting for the current major zone (Orgrimmar) or sub-zone (Valley of Strength).')
 	end
 
+	if args == 'blacklist' then
+		print('blacklist (currentzone | currentsubzone) - toggle blacklisting for the current major zone (Orgrimmar) or sub-zone (Valley of Strength).')
+	end
+
+	if args == 'vo' then
+		print ('vo (on | off | toggle) - enable or disable removal of vo seperate from hiding talking head ui')
+	end
+
 	if args == '' then
 		print('BeQuiet version ' .. version)
-		print('Options: on | off | toggle | verbose | whitelist | reset | delete | show')
+		print('Options: on | off | toggle | verbose | whitelist | blacklist | reset | delete | show | vo')
 		print('-----')
 		if ENABLED == 1 then
 			print('BeQuiet is currently enabled.')
--- README.md
+++ README.md
@@ -13,10 +13,14 @@ Functionality:
 
 2. '/bq whitelist (currentzone | currentsubzone)' This will allow talking heads in your current major zone or sub-zone. The zones are dictated by where you are in the game, but an example for a Major zone would be Orgrimmar and an example of the sub-zone would be the Valley of Strength. This should help users quickly whitelist areas based on their immediate needs and preferences and won't require me to maintain a full whitelist. Use the command once to add the zone to the whitelist and a second time to remove it.
 
-3. '/bq reset' will reset the whitelist back to the withered army training, island expedition zones, and torghast only.
+3. '/bq blacklist (currentzone | currentsubzone)' Similar to the whitelist command, this will block the current zone or sub-zone always. Even when bq is set to off. Just like whitelist the command toggles. It defaults to empty.
 
-4. '/bq show' will print the current list of whitelisted zones to your chatbox.
+4. '/bq reset' will reset the whitelist back to the withered army training, island expedition zones, and torghast only. This will also reset the blacklist back to empty.
 
-5. '/bg verbose' will print a message in your chat box indicating that a talking head was blocked. I'm adding this with the Shadowlands release for some players who might not realize the talking heads are being blocked when new content releases. Run the command to toggle it on or off.
+5. '/bq show' will print the current list of whitelisted zones to your chatbox.
 
-6. '/bq delete' will totally delete the whitelist.
+6. '/bg verbose' will print a message in your chat box indicating that a talking head was blocked. I'm adding this with the Shadowlands release for some players who might not realize the talking heads are being blocked when new content releases. Run the command to toggle it on or off.
+
+7. '/bq delete' will totally delete the whitelist and blacklist.
+
+8. '/bq vo' will allow the vo to continue when the ui frame is hidden.
